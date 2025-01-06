dd13443979bc4894f8c9bd34222b2856b7e75bdb
Salman Bahaev <bahaev.sm@gmail.com>
2025-01-06 14:33:45 +0300

    added classes

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,14 +1,21 @@
 import React from "react";
 
 const Counter = () => {
-  const count = 0
+  const count = 0;
   const formatCount = () => {
-    return count === 0 ? "empty" : count
-  }
+    return count === 0 ? "empty" : count;
+  };
+
+  const getBadgeClasses = () => {
+    let classes = "badge m-2 ";
+    classes += count === 0 ? "bg-warning" : "bg-primary";
+    return classes;
+  };
+
   return (
     <>
-      <h1>{formatCount()}</h1>
-      <button>+</button>
+      <span className={getBadgeClasses()}>{formatCount()}</span>
+      <button className="btn btn-primary btn-sm m-2">+</button>
     </>
   );
 };
